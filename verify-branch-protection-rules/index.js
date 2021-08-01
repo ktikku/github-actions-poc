22fc8205efb5a9607ca699ac718aa96302812cc9
Karan Tikku <50620678+c285803@users.noreply.github.com>
2021-08-01 12:12:06 +0530

    initial commit

---
+++ verify-branch-protection-rules/index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const myToken = core.getInput('my-token');
+    const octokit = github.getOctokit(myToken);
+    const result = await octokit.graphql(`
+    {
+      repository(owner: "EliLillyCo", name: "testing-github-actions") {
+        branchProtectionRules(first: 100) {
+          nodes {
+            pattern
+            requiredApprovingReviewCount
+          }
+        }
+      }
+    }    
+  `);
+    console.log(result.repository);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
